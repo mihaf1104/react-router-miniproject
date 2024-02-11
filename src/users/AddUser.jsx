b8558cacdb320b38d8f1179795a73c87b2201b5d
mihaf <fahimeh2067@gmail.com>
2024-02-11 23:52:38 +0330

    Delete Method

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -15,40 +15,40 @@ const AddUser = () => {
             </h4>
             <div className="row justify-content-center mt-5 ">
                 <form className="col-12 col-md-6 bg-light rounded shadow-lg p-3">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">نام و نام خانوادگی</label>
-                        <input type="text" class="form-control" />
+                    <div className="mb-3">
+                        <label  className="form-label">نام و نام خانوادگی</label>
+                        <input type="text" className="form-control" />
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">نام کاربری</label>
-                        <input type="text" class="form-control" />
+                    <div className="mb-3">
+                        <label  className="form-label">نام کاربری</label>
+                        <input type="text" className="form-control" />
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">ایمیل</label>
-                        <input type="email" class="form-control" />
+                    <div className="mb-3">
+                        <label  className="form-label">ایمیل</label>
+                        <input type="email" className="form-control" />
                     </div>
-                    <div class="mb-3 row">
-                        <label for="exampleInputEmail1" class="form-label">آدرس</label>
+                    <div className="mb-3 row">
+                        <label  className="form-label">آدرس</label>
                         <div className="col-6 my-1">
-                            <input type="text" class="form-control" placeholder="شهر" />
+                            <input type="text" className="form-control" placeholder="شهر" />
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" class="form-control" placeholder="خیابان" />
+                            <input type="text" className="form-control" placeholder="خیابان" />
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" class="form-control" placeholder="ادامه آدرس" />
+                            <input type="text" className="form-control" placeholder="ادامه آدرس" />
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" class="form-control" placeholder="کد پستی" />
+                            <input type="text" className="form-control" placeholder="کد پستی" />
                         </div>
                     </div>
 
                     <div className="col-12 text-start">
                         <Link to="/users">
-                            <button type="button" class="btn btn-danger ms-2"
+                            <button type="button" className="btn btn-danger ms-2"
                             onClick={()=> navigate(-1)}>بازگشت</button>
                         </Link>
-                        <button type="submit" class="btn btn-primary" >
+                        <button type="submit" className="btn btn-primary" >
                             {userId ? "ویرایش " : "افزودن "}
                         </button>
                     </div>
--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,11 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import style from '../style.module.css'
 import { Link, useNavigate } from 'react-router-dom';
-import AddUser from './AddUser';
-
+import swal from 'sweetalert';
+import axios from 'axios';
 const Users = () => {
 
     const navigate = useNavigate();
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => { setUsers(res.data) })
+            .catch(err => { })
+            console.log("useeffect")
+    }, [])
+
+    const handleDelete = (itemId) => {
+        swal({
+            title: "حذف رکورد!",
+            text: `آیا از حذف رکورد ${itemId} مطمئن هستید ؟`,
+            icon: "warning",
+            dangerMode: true,
+            buttons: ["خیر", "بله"],
+        }).then((willDelete) => {
+            if (willDelete) {
+
+                axios({
+                    method: "DELETE",
+                    url: `https://jsonplaceholder.typicode.com/users/${itemId}`
+
+                }).then(res => {
+                    console.log(res)
+                    if (res.status === 200) {
+                       console.log("200")
+                        let newUsers = users.filter(a => a.id !== itemId);
+                        setUsers(newUsers)
+                    }
+                })
+                    .catch(err => { console.log(err); })
+                // axios.delete(`https://jsonplaceholder.typicode.com/users/${itemId}`)
+                swal("حذف با موفقیت انجام شد", { icon: "success", buttons: "متوجه شدم" })
+
+            } else { swal("شما از حذف رکورد منصرف شدید") }
+        }
+        )
+    }
     return (
         <Fragment>
             <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
@@ -23,32 +62,47 @@ const Users = () => {
                         </Link>
                     </div>
                 </div>
-                <table className="table bg-light shadow">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>نام</th>
-                            <th>نام کاربری</th>
-                            <th>ایمیل</th>
-                            <th>عملیات</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>qasem</td>
-                            <td>qasemB</td>
-                            <td>mahdicmptr@gmail.com</td>
-                            <td>
-                                <i className="fas fa-edit text-warning mx-2 pointer"
-                                    onClick={() => navigate("/users/add/2")}
-                                ></i>
+                {
+                    users.length ?
+                        (
 
-                                <i className="fas fa-trash text-danger mx-2 pointer"></i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                            <table className="table bg-light shadow">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>نام</th>
+                                        <th>نام کاربری</th>
+                                        <th>ایمیل</th>
+                                        <th>عملیات</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {users.map((u) =>
+                                        <tr key={u.id}>
+                                            <td>{u.id}</td>
+                                            <td>{u.name}</td>
+                                            <td>{u.username}</td>
+                                            <td>{u.email}</td>
+                                            <td>
+                                                <i className="fas fa-edit text-warning mx-2 pointer"
+                                                    onClick={() => navigate(`/users/add/${u.id}`)}
+                                                ></i>
+
+                                                <i className="fas fa-trash text-danger mx-2 pointer"
+                                                    onClick={() => handleDelete(u.id)}></i>
+                                            </td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+
+
+                        )
+                        : (
+
+                            <h4 className="text-center text-info" >لطفا صبر کنید ...</h4>
+                        )
+                }
             </div>
         </Fragment>
 
